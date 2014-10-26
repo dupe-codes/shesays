5b99069d0282eb5888d250a90fd502b4c59ba9f7
njdup <njdupp@gmail.com>
2014-10-25 23:14:49 -0700

    Added machinery to create new companies

--- shesays_platform/apps/companies/views.py
+++ shesays_platform/apps/companies/views.py
@@ -3,9 +3,24 @@ Logic for handling and interacting with
 companies
 """
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from models import Company
 
 def display_company(request, company_id):
     """ Displays a single company's profile page """
     return render_to_response('companies/company_profile.html', {'company_id': company_id})
+
+def new_company(request):
+    """ Renders view for creating a new company """
+    return render_to_response('companies/new_company.html')
+
+def create_company(request):
+    """ Creates a new company given form information """
+    if request.method == 'POST':
+        parameters = request.POST
+        new_company = Company(parameters)
+        new_company.save()
+
+        return redirect('/company/{}'.format(new_company.id))
+    else:
+        return redirect('/')
