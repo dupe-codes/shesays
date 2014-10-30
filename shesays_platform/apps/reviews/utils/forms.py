0f91f0eb719c95006f49950814416625ff9a98de
Sherman Leung <shermankleung@gmail.com>
2014-10-30 01:29:54 -0700

    fixed the styling on the reviews

--- shesays_platform/apps/reviews/utils/forms.py
+++ shesays_platform/apps/reviews/utils/forms.py
@@ -7,5 +7,5 @@ from .. import settings #TODO: Replace this with import of django settings modul
 
 class ReviewForm(Form):
     """ form for creating a new review. """
-    content = CharField(widget=Textarea, initial='Add your review')
+    content = CharField(widget=Textarea(attrs={'placeholder': 'Add your review here'}))
     sentiment = ChoiceField(choices=((label,label) for label in settings.SENTIMENT_LABELS))
